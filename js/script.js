a464266283384503ff224ed25ded70fde2b2108e
Hutasako <ejjungao@yahoo.com>
2021-04-27 01:01:33 -0400

    css updates, added js file

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// get products container
+const resultsContainer = document.querySelector('.results');
+
+// make an array of products
+const products = [
+    {
+        'name': 'X',
+        'type': 'Y',
+        'desc': 'Test',
+        'options': ['1.5', '5'],
+        'imgs': '',
+        'price': {
+            'sale': 'true', 
+            'salePrice':'19',
+            'current':'30',
+        },
+    },
+    {
+        'name': 'Y',
+        'type': 'X',
+        'desc': 'Test',
+        'options': '',
+        'imgs': '',
+        'price': {
+            'sale': 'true', 
+            'salePrice':'19',
+            'current':'30',
+        },
+    },
+]
+
+
+
+function getOptions(options){
+    if(options){
+        return `
+        <form class="product-size pad-content">
+        <fieldset>
+        <legend>Sizes</legend>
+        <ol>
+        ${options.map( opt => `<li><label><input type="radio" name="weight" value="${opt}"> ${opt}g</label></li>`).join("")}
+        </ol>
+        </fieldset>
+        </form>
+        `
+    }
+    else{
+        return ''
+    }
+}
+products.forEach((item)=>{
+    const productContainer = document.createElement('article');
+    productContainer.classList.add('product');
+    productContainer.innerHTML = 
+    `
+    <header>
+    <img src="img/oregano.jpg" alt="Product Image">
+    <button type="button" class="fav-btn">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M0 0h24v24H0V0z" fill="none"/>
+    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+    </svg>
+    </button>
+    <div class="pad-content">
+    <h3>${item.name}</h3>
+        ${item.price.sale == 'true'
+            ? `<data value="${item.price.salePrice}"><del>$${item.price.current}</del> <ins>$${item.price.salePrice}</ins></data>` 
+            : `<data value="${item.price.current}"><ins>$${item.price.current}</ins></data>`}
+    <p>${item.desc}</p>
+    <dl>
+    <dt>Rating</dt>
+    <dd>4.4 <span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star</span><span class="material-icons">star_half</span></dd>
+    </dl>
+    <a href="#">see more</a>
+    </div>
+    </header>
+    ${getOptions(item.options)}     
+    <footer>
+    <button type="button" class="add-cart">Add to Cart</button>
+    </footer>
+    `;
+    console.log(item)
+    console.log(productContainer.innerHTML)
+    resultsContainer.appendChild(productContainer)
+    })
